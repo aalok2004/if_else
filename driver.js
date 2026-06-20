c1e95d719235b147d74206719f81bc922991e4f4
aalok2004 <raghuvanshiaalok@gmail.com>
2026-06-20 16:26:40 +0530

    driver eligible

---
+++ driver.js
@@ -0,0 +1,20 @@
+var a ="unmarried";
+var gender="female";
+var age =40;
+if(a=="married")
+    {
+        console.log("you are can't drive");
+
+    }else
+        {
+          
+            if(gender=="male"&&age>=40)
+                {
+                    console.log("you can drive");
+                }
+            if(gender=="female"&&age>=30) 
+                {
+                    console.log("you can drive ");
+                }
+
+        }
