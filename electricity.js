d57e5b23686646dc186698faeabe01eb83ccd009
aalok2004 <raghuvanshiaalok@gmail.com>
2026-06-22 14:07:31 +0530

    electricity bill

--- electricity.js
+++ electricity.js
@@ -1,12 +1,15 @@
 let units = parseFloat(456);
+
 let bill;
 
 if (units <= 50) {
     bill = units * 0.50;
 }
+
 else if (units <= 150) {
     bill = (50 * 0.50) + ((units - 50) * 0.75);
 }
+
 else if (units <= 250) {
     bill = (50 * 0.50) + (100 * 0.75) + ((units - 150) * 1.20);
 }
